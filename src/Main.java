d562760710cc811b00a3dfac65400fdb879f797e
Bolot Gonchikov <gonchikovbb@gmail.com>
2021-11-26 12:54:11 +0700

    Modified if-else

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,17 @@ public class Main {
     public static void main(String args[]){
         int StartCheck = 100; // Начальный счет
         int Sum_add = 1500; // Сумма пополнения баланса
+        int Bonus; //Бонус
         if( Sum_add < 1000 ){
-            System.out.print("Итого: ");
-            System.out.print(Sum_add+StartCheck);
-            System.out.print(" руб.");
-            System.out.print(" Бонус: ");
-            System.out.print("0");
-            System.out.print(" руб.");
+            Bonus = 0;
         }else{
-            int Bonus = Sum_add / 100;
-            System.out.print("Итого: ");
-            System.out.print(Sum_add+StartCheck+Bonus);
-            System.out.print(" руб.");
-            System.out.print(" Бонус: ");
-            System.out.print(Bonus);
-            System.out.print(" руб.");
+            Bonus = Sum_add / 100;
         }
+        System.out.print("Итого: ");
+        System.out.print(Sum_add+StartCheck+Bonus);
+        System.out.print(" руб.");
+        System.out.print(" Бонус: ");
+        System.out.print(Bonus);
+        System.out.print(" руб.");
     }
 }
